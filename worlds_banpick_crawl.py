0637f8f9072669dfea197522b714846891f791f4
andjuly1 <uvhs@kangwon.ac.kr>
2024-11-19 23:30:15 +0900

    날짜, 토너먼트/세트, 팀명 추출

--- worlds_banpick_crawl.py
+++ worlds_banpick_crawl.py
@@ -2,29 +2,49 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-def crawl(base_url, pages):
-    data = []
+# 월즈 데이터 크롤링
+def worlds_crawl():
+    worlds_data = []
 
-    for page in range(1, pages + 1):
-        url = f"{base_url}?pg={page}&iskin=lol&category2=192"
+    for page in range(1, 5):
+        url = "https://lol.inven.co.kr/dataninfo/match/teamList.php?pg={page}&iskin=lol&category2=192"
+        print(f"페이지 {page} 크롤링")
 
         response = requests.get(url)
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, "html.parser")
-    
-        db = soup.find("pick", class_="listframe") 
-        if not db:
-            print("find 실패")
-            continue
+        matchlist = soup.find_all("div", class_="listFrame")
 
-    return data
+        for match in matchlist:
+            match_data = {}
+
+            # 날짜 
+            date_tag = match.find("div", class_="date")
+            match_data["date"] = date_tag.text.strip() 
+
+            # 토너먼트명 및 세트 숫자 추출
+            stage_tag = match.find("div", class_="stage")
+            match_data["stage"] = stage_tag.text.strip()
+
+            # wTeam 추출
+            wteam_tag = match.find("div", class_="wTeam")
+            if wteam_tag:
+                w_team_name = wteam_tag.find("a", class_="teamname")
+                match_data["wTeam"] = w_team_name.text.strip() 
+
+            # lTeam 추출
+            lteam_tag = match.find("div", class_="lTeam")
+            if lteam_tag:
+                l_team_name = lteam_tag.find("a", class_="teamname")
+                match_data["lTeam"] = l_team_name.text.strip() 
+
+            worlds_data.append(match_data)
+
+    return worlds_data
 
 if __name__ == "__main__":
-    base_url = "https://lol.inven.co.kr/dataninfo/match/teamList.php"
-    pages = 4 # 페이지 4까지 크롤링
-
-    data = crawl(base_url, pages)
+    worlds_data = worlds_crawl()
 
     with open("worlds_banpick_data.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False)\
+        json.dump(worlds_data, f, ensure_ascii=False, indent=4)
